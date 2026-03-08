b7da233c68774f3855d2c86d142c0a397f23fb84
Brahim El bourkali <brel-bou@c1r7p7.1337.ma>
2026-03-08 15:45:40 +0000

    .

--- ex3/ft_vault_security.py
+++ ex3/ft_vault_security.py
@@ -1,20 +1,28 @@
 #!/usr/bin/env python3
 
-def main():
+def main() -> int:
     print("=== CYBER ARCHIVES - VAULT SECURITY SYSTEM ===\n")
     print("Initiating secure vault access...")
-    print("Vault connection established with failsafe protocols")
-    print("\nSECURE EXTRACTION:\n")
-    with open("test.txt", "r+") as file:
+    print("Vault connection established with failsafe protocols\n")
+    print("SECURE EXTRACTION:")
+
+    with open("test.txt", "r") as file:
         data = file.read()
-        print(f"{data}")
-        list = ["[CLASSIFIED] New security protocols archived",
-                "Vault automatically sealed upon completion"]
-        for line in list:
+    print(f"{data}")
+    print("\nSECURE PRESERVATION:")
+    with open("test.txt", "w") as file:
+        lista = ["[CLASSIFIED] New security protocols archived",
+                 "Vault automatically sealed upon completion"]
+        for line in lista:
             file.write(line)
             file.write("\n")
-        dataa = file.read()
-        print(f"{dataa}")
+
+    for line in lista:
+        print(line)
+
+    print("\nAll vault operations completed with maximum security.")
+    return 0
 
 
-main()
+if __name__ == "__main__":
+    main()
